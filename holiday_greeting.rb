5594df9762dd5053538c1687b7ba8064ae8d7a2a
nicklennon <nicklennon.53@gmail.com>
2018-06-17 16:53:39 +0000

    Done.

---
+++ holiday_greeting.rb
@@ -0,0 +1,8 @@
+#Holiday Greeting Method where a is holiday, b is recipient, and c is sender
+def holiday_greeting(a = "Mother's Day", b = "Mom", c = "Your Favorite Child")
+  "Happy #{a}, #{b}!  - From #{c}"
+end
+
+#Test Cases
+puts holiday_greeting
+puts holiday_greeting("Fourth of July","Beyonce","Jay-Z")
