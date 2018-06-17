b7c6af3a0f2450781a5cccef6cb7b46c028e604f
nicklennon <nicklennon.53@gmail.com>
2018-06-17 16:53:16 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,6 +1,6 @@
 #Holiday Greeting Method where a is holiday, b is recipient, and c is sender
 def holiday_greeting(a = "Mother's Day", b = "Mom", c = "Your Favorite Child")
-  "Happy #{a}, #{b}! From #{c}"
+  "Happy #{a}, #{b}! - From #{c}"
 end
 
 #Test Cases
